841ea36f0ed4b4b009b2fbebb3df8fffa3c6398e
rahulDTS <rahul.yadav@dtskill.com>
2026-01-28 16:15:36 +0530

    updated explore code

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,14 @@
 import AgentData from "@/component/AgentData";
 import CreateAgent from "@/component/CreateAgent";
 import { AgentContext } from "@/context/AgentContext";
+import { useRouter } from "expo-router";
 import { ArrowRight } from "lucide-react-native";
 import React, { useContext } from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const Explore = () => {
   const { createdAgentData } = useContext(AgentContext);
-
+  const router = useRouter();
   return (
     <View className="p-5">
       <CreateAgent />
@@ -16,16 +17,26 @@ const Explore = () => {
       <View className="flex-col gap-1 my-4">
         <Text className="text-2xl font-bold">My Agent/Assistant</Text>
         {createdAgentData.map((item, index) => (
-          <View
+          <TouchableOpacity
             key={index}
             className="flex-row items-center justify-between p-4 border border-LIGHT_GRAY bg-WHITE rounded-xl"
+            onPress={() =>
+              router.push({
+                pathname: "/chat",
+                params: {
+                  agentName: item.agentName,
+                  initialText: "",
+                  initialPrompt: item.initialPrompt,
+                },
+              })
+            }
           >
             <View className="flex-row gap-4">
               <Text className="text-2xl">{item.emoji}</Text>
               <Text className="text-[19px] font-medium">{item.agentName}</Text>
             </View>
             <ArrowRight />
-          </View>
+          </TouchableOpacity>
         ))}
       </View>
 
